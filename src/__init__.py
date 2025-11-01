89a82e818923bdd769eaf0ac58e81ac9f0c141d6
Ouday Khaled <oudaykhaled@oudays-mbp.home>
2025-11-01 18:05:15 +0100

    🔧 Fix import error after removing SubtitleGenerator
    - Remove SubtitleGenerator from import in __init__.py
    - Fix 'ImportError: cannot import name SubtitleGenerator'

--- src/__init__.py
+++ src/__init__.py
@@ -1,6 +1,6 @@
 from .subtitle_part import SubtitlePart
 from .subtitle_webvtt_parser import SubtitleWebVTTParser
-from .subtitle_segment_finder import SubtitleGenerator, SubtitleSegmentFinder
+from .subtitle_segment_finder import SubtitleSegmentFinder
 from .subtitle_srt_parser import SubtitleSRTParser
 from .time_utils import (
     convert_clock_time_to_timestamp_ms,
